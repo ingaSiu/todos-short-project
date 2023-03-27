3c297d18887ce8f8d54d96b26196a45991f9deca
ingaSiu <ingasiud@gmail.com>
2023-03-27 12:15:04 +0300

    get ir post todo

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,20 @@
 import { Todo } from '../types/type';
 import axios from 'axios';
 
-export const ferchTodos = (): Promise<Todo[]> => {
-  return axios('http://localhost:8080/').then((response) => response.data);
+const TODOS_API_URL = 'http://localhost:3001/';
+
+export const fetchTodos = (): Promise<Todo[]> => {
+  return axios.get(TODOS_API_URL).then((response) => {
+    console.log('SUCCESS getPost');
+    console.log(response.data);
+    return response.data;
+  });
+};
+
+export const postTodo = (todo: Todo): Promise<Todo[]> => {
+  return axios.post(TODOS_API_URL, todo).then((response) => {
+    console.log('SUCCESS insertTodo');
+    console.log(response.data);
+    return response.data;
+  });
 };
