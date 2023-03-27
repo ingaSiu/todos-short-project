93c3e59082a126ac31afb4d62cf76e26699061e3
ingaSiu <ingasiud@gmail.com>
2023-03-27 12:13:46 +0300

    display todo list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,17 @@
+import { useCreateTodo, useTodos } from './hooks/todos';
 import { useEffect, useState } from 'react';
 
 import { Todo } from './types/type';
-import { ferchTodos } from './api/api';
+import { fetchTodos } from './api/api';
 
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  useEffect(() => {
-    ferchTodos().then((response) => {
-      setTodos(response);
-    });
-  }, []);
+  const { data } = useTodos();
+  const todos = data || [];
+
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <li key={todo._id}>{todo.title}</li>
       ))}
     </ul>
   );
